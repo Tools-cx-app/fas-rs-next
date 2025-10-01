49bfcee02712bb12c6d12a77f70383b01a81c48f
Tools-app <localhost.hutao@gmail.com>
2025-10-01 19:50:18 +0800

    feat(module): add toast
    Signed-off-by: Tools-app <localhost.hutao@gmail.com>

--- module/service.sh
+++ module/service.sh
@@ -20,18 +20,25 @@ MODDIR=${0%/*}
 DIR=/sdcard/Android/fas-rs
 MERGE_FLAG=$DIR/.need_merge
 LOG=$DIR/fas_log.txt
+LANGUAGE=$(getprop persist.sys.locale | cut -d'-' -f1)
 
 sh $MODDIR/vtools/init_vtools.sh $(realpath $MODDIR/module.prop)
 
 until [ -d $DIR ]; do
-	sleep 1
+  sleep 1
 done
 
 if [ -f $MERGE_FLAG ]; then
-	$MODDIR/fas-rs-next merge $MODDIR/games.toml >$DIR/.update_games.toml
-	rm $MERGE_FLAG
-	mv $DIR/.update_games.toml $DIR/games.toml
+  $MODDIR/fas-rs-next merge $MODDIR/games.toml >$DIR/.update_games.toml
+  rm $MERGE_FLAG
+  mv $DIR/.update_games.toml $DIR/games.toml
 fi
 
 killall fas-rs-next
 RUST_BACKTRACE=1 nohup $MODDIR/fas-rs-next run $MODDIR/games.toml >$LOG 2>&1 &
+
+if [ "$LANGUAGE" = "zh" ]; then
+  su -lp 2000 -c "cmd notification post -S bigtext -t 'FAS-RS-NEXT' 'Tag' '感谢使用fas-rs-next，滑动以删除本通知。Powered by Tools-cx-app'"
+else
+  su -lp 2000 -c "cmd notification post -S bigtext -t 'FAS-RS-NEXT' 'Tag' 'Thanks for using fas-rs-next, swipe to remove this notification.Powered by Tools-cx-app'"
+fi
