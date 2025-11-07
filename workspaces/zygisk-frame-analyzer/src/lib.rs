3638886da16978d1fe2d4bc7aea1e534b4ed8e73
Tools-app <localhost.hutao@gmail.com>
2025-11-07 18:44:31 +0800

    feat(zygisk): Improve dump logic
    Signed-off-by: Tools-app <localhost.hutao@gmail.com>

--- workspaces/zygisk-frame-analyzer/src/lib.rs
+++ workspaces/zygisk-frame-analyzer/src/lib.rs
@@ -1,9 +1,11 @@
 use std::{
+    io::Read,
     os::unix::net::UnixStream,
     path::{Path, PathBuf},
+    time::Duration,
 };
 
-use anyhow::Result;
+use anyhow::{Context, Result};
 
 pub struct Analyzer {
     sock_addr: PathBuf,
@@ -21,10 +23,40 @@ impl Analyzer {
         }
     }
 
-    pub fn connect(&mut self) -> Result<&mut Self> {
+    pub fn connection(&mut self) -> Result<&mut Self> {
         self.connect = Some(UnixStream::connect(self.sock_addr.clone())?);
 
         Ok(self)
+    }
+
+    pub fn dump(&mut self, timeout: u64) -> (String, i32) {
+        let mut connect = {
+            if let Some(connect) = &self.connect {
+                connect
+                    .try_clone()
+                    .context("Failed to clone socket connection")
+                    .unwrap()
+            } else {
+                return (String::new(), 0);
+            }
+        };
+        let mut buffer = String::new();
+
+        connect
+            .set_read_timeout(Some(Duration::from_secs(timeout)))
+            .context("Failed to set timeout")
+            .unwrap();
+        connect
+            .read_to_string(&mut buffer)
+            .context("Failed to read socket")
+            .unwrap();
+
+        let mid = buffer.find(":").unwrap();
+
+        {
+            let (frametime, pid) = buffer.split_at(mid);
+            (frametime.to_string(), pid.parse::<i32>().unwrap())
+        }
     }
 }
 
@@ -34,7 +66,7 @@ mod tests {
 
     #[test]
     fn sock_connect() -> Result<()> {
-        Analyzer::new("./test.sock").connect()?;
+        Analyzer::new("./test.sock").connection()?;
 
         Ok(())
     }
